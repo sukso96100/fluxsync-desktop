a74e91a25c49c143ccfb43cedd6221261ab49499
sukso96100 <sukso96100@gmail.com>
2017-06-26 15:34:42 +0900

    working with router

--- src/rend/shell.js
+++ src/rend/shell.js
@@ -3,25 +3,26 @@ module.paths.push(
   __dirname + '/../res/img',
 __dirname + '/../res/layout');
 
-const Vue = require('vue/dist/vue.js')
-const VueRouter = require('vue-router')
+const Vue = require('vue/dist/vue.js');
+const VueRouter = require('vue-router');
+
 Vue.use(VueRouter)
 
-const Device = { template: require('./device.html').layout }
-const Clipboard = { template: require('./clipboard.html').layout }
-const Sms = { template: require('./sms.html').layout }
-const Settings = { template: require('./settings.html').layout }
+const Device = { template: require('./device.html').layout };
+const Clipboard = { template: require('./clipboard.html').layout };
+const Sms = { template: require('./sms.html').layout };
+const Settings = { template: require('./settings.html').layout };
 const routes = [
   { path:'/device', component:Device },
   { path:'/clipboard', component:Clipboard },
   { path:'/sms', component: Sms },
   { path:'/settings', component: Settings }
-]
+];
 
 const router = new VueRouter({
   routes
-})
+});
 
 const app = new Vue({
   router
-}).$mount('#app')
+}).$mount('#app');
