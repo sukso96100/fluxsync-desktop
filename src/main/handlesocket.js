ac94a45eb3dcaddc322cedfef8d552372ea9ba4a
nara <nr970810@gmail.com>
2018-03-20 19:23:05 +0900

    [feature/message] desktop mirroring (#5)

--- src/main/handlesocket.js
+++ src/main/handlesocket.js
@@ -2,6 +2,13 @@ const notifier = require('node-notifier');
 
 exports.handleSocket = function(socket){
   socket.on('notify', (data)=>{
-    notifier.notify(data);
+     let json = JSON.parse(data);
+     let title = json.title;
+     let content = json.content; //json 파싱으로 title과 content 추출
+
+     notifier.notify({
+       title : title
+       content : content
+     })  //추출한 title과 content를 notify의 인자로 넘김
   })
 }
