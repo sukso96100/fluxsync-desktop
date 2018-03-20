82240265a59a45e0b8ec177e2449ca0a793f5e1a
nara <nr970810@gmail.com>
2018-03-20 21:34:48 +0900

    [feature/message] add comma (#5)

--- src/main/handlesocket.js
+++ src/main/handlesocket.js
@@ -2,13 +2,15 @@ const notifier = require('node-notifier');
 
 exports.handleSocket = function(socket){
   socket.on('notify', (data)=>{
+    console.log(data);
+    console.log(typeof data);
      let json = JSON.parse(data);
      let title = json.title;
      let content = json.content; //json 파싱으로 title과 content 추출
 
      notifier.notify({
        title : title,
-       content : content
+       message : content
      })  //추출한 title과 content를 notify의 인자로 넘김
   })
 }
