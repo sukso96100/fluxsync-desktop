137cd280269b8619f0c2f7c3dfd45a33b845b935
sukso96100 <sukso96100@gmail.com>
2018-05-27 12:47:44 +0900

    MacOS Notification Actoins 표시 구현(#8)

--- src/main/handlesocket.js
+++ src/main/handlesocket.js
@@ -1,4 +1,5 @@
 const { Notification } = require('electron');
+const os = require('os');
 
 exports.handleSocket = function(socket){
 
@@ -7,15 +8,46 @@ exports.handleSocket = function(socket){
   socket.on('notify', (data)=>{
     console.log(data);
     console.log(typeof data);
-     let json = JSON.parse(data);
-     let title = json.title;
-     let content = json.content; //json 파싱으로 title과 content 추출
+    let json = JSON.parse(data);
+    let title = json.title;
+    let content = json.content; //json 파싱으로 title과 content 추출
+    let actions = [];
+    let mobileNoti;
 
-     let mobileNoti = new Notification({
-       title: title,
-       body: content
-     });
-
+    if(json.actions == undefined){
+      mobileNoti = new Notification({
+        title: title,
+        body: content
+      });
+    }else{
+      json.actions
+      .forEach((item)=>{
+        console.log(item);
+        actions.push({
+          type: "button",
+          text: item.toString()
+        })
+      });
+      console.log(actions);
+      switch(os.platform()){
+        case 'darwin':
+          // MacOS
+          mobileNoti = new Notification({
+            title: title,
+            body: content,
+            actions: actions
+          });
+          break;
+        case 'win32':
+          // Windows
+          break;
+        case 'linux':
+          // Linux Desktop
+          break;
+          
+      }
+    }
+    
      mobileNoti.show();
      // notifier.notify({
      //   title : title,
